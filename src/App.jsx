d9a1d905f0001b28869e02931fb419ce8a0fbad3
Christian Wu <christian.wu@galvanize.com>
2023-10-31 12:58:55 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <div className='title-container'>
+          <h1 className='main-home-title'><b>Hello</b></h1>
+          <button>hello</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
